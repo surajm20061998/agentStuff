252d59d4b15952594cc32ed28037688a1d1c2aac
surajm20061998 <surajm20061998@gmail.com>
2026-09-01 13:20:03 -0400

    Init

---
+++ soloAgents/main.py
@@ -0,0 +1,83 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+
+from tools import search_tool, wiki_tool, save_tool
+
+
+load_dotenv()
+
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+
+llm = ChatOllama(
+    model="qwen3:4b"
+)
+
+parser = PydanticOutputParser(
+    pydantic_object=ResearchResponse
+)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use necessary tools.
+
+            Wrap the output in this format and provide no other text:
+
+            {format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(
+    format_instructions=parser.get_format_instructions()
+)
+
+
+tools = [search_tool, wiki_tool, save_tool]
+
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools
+)
+
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True
+)
+
+query = input("What can I help you research?")
+
+raw_response = agent_executor.invoke(
+    {
+        "query": query
+    }
+)
+
+#print(raw_response)
+
+try :
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
+except Exception as e:
+    print("Error Parsing Response ", e, "Raw Response - ", raw_response)
